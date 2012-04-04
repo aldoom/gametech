539ac55f1d1916ebb1c244bb59adddce5cb061e4
Alexander Sukach <aldoom1@gmail.com>
2012-04-04 15:16:21 +0300

    Первый коммит фичи темы + фунционал для работы с куками.

--- NewsCommentsHomerAndSamuraiJack.user.js
+++ NewsCommentsHomerAndSamuraiJack.user.js
@@ -20,6 +20,424 @@ function addJQuery(callback) {
 }
 
 function main() {
+    /* store params/settings in coockies */
+    /**
+     * Example:
+     * GET
+     * gtnamespace.cookies.get('myCookie');
+     *   returns value of myCookie if it is present, null if not 
+     * gtnamespace.cookies.get(['myCookie', 'myOtherCookie']);
+     *   returns array containing value of each requested cookie if it is present, null if not 
+     * gtnamespace.cookies.get();
+     *   returns array of all cookies from your site
+     * 
+     * 
+     * FILTER
+     * gtnamespace.cookies.filter( /^site/ );
+     *   returns list of cookies whose names start with "site" 
+     * 
+     * 
+     * SET
+     * gtnamespace.cookies.set('myCookie', 'myValue');
+     *   sets cookie by the name of 'myCookie' to value of 'myValue' with default options 
+     * gtnamespace.cookies.set('myCookie', 'myValue', {path: '/somedir'});
+     *   sets cookie by the name of 'myCookie' to value of 'myValue' with path of '/somedir' 
+     * 
+     * 
+     * DELETE
+     * gtnamespace.cookies.del('myCookie');
+     *   deletes a cookie, 'myCookie', with default options 
+     * gtnamespace.cookies.del('myCookie', {path: '/somedir'});
+     *   deletes a cookie by the name of 'myCookie' which had been set with a path of '/somedir' 
+     * gtnamespace.cookies.del(true);
+     *   deletes all cookies 
+     * 
+     * 
+     * TEST
+     * gtnamespace.cookies.test();
+     *   attempts to set a cookie and returns true or false upon success or failure 
+     * 
+     * 
+     * SET OPTIONS
+     * gtnamespace.cookies.setOptions({path: '/somedir'});
+     *   all cookies will be set or deleted with the path , '/somedir', unless it is explicitly provided in a passed options object 
+     * 
+     * 
+     * 
+     */
+    var gtnamespace = window.gtnamespace || {};
+    gtnamespace.cookies = ( function() {
+        var resolveOptions, assembleOptionsString, parseCookies, constructor, defaultOptions = {
+            expiresAt: null,
+            path: '/',
+            domain:  null,
+            secure: false
+        };
+        
+        /**
+        * resolveOptions - receive an options object and ensure all options are present and valid, replacing with defaults where necessary
+        *
+        * @access private
+        * @static
+        * @parameter Object options - optional options to start with
+        * @return Object complete and valid options object
+        */
+        resolveOptions = function( options )
+        {
+            var returnValue, expireDate;
+
+            if( typeof options !== 'object' || options === null )
+            {
+                returnValue = defaultOptions;
+            }
+            else
+            {
+                returnValue = {
+                    expiresAt: defaultOptions.expiresAt,
+                    path: defaultOptions.path,
+                    domain: defaultOptions.domain,
+                    secure: defaultOptions.secure
+                };
+
+                if( typeof options.expiresAt === 'object' && options.expiresAt instanceof Date )
+                {
+                    returnValue.expiresAt = options.expiresAt;
+                }
+                else
+                {
+                    expireDate = new Date();
+                    expireDate.setTime( expireDate.getTime() + ( 365 * 24 * 60 * 60 * 1000 ) );
+                    returnValue.expiresAt = expireDate;
+                }
+
+                if( typeof options.path === 'string' && options.path !== '' )
+                {
+                    returnValue.path = options.path;
+                }
+
+                if( typeof options.domain === 'string' && options.domain !== '' )
+                {
+                    returnValue.domain = options.domain;
+                }
+
+                if( options.secure === true )
+                {
+                    returnValue.secure = options.secure;
+                }
+            }
+
+            return returnValue;
+        };
+        
+        /**
+        * assembleOptionsString - analyze options and assemble appropriate string for setting a cookie with those options
+        *
+        * @access private
+        * @static
+        * @parameter options OBJECT - optional options to start with
+        * @return STRING - complete and valid cookie setting options
+        */
+        assembleOptionsString = function( options )
+        {
+            options = resolveOptions( options );
+
+            return (
+                ( typeof options.expiresAt === 'object' && options.expiresAt instanceof Date ? '; expires=' + options.expiresAt.toGMTString() : '' ) +
+                '; path=' + options.path +
+                ( typeof options.domain === 'string' ? '; domain=' + options.domain : '' ) +
+                ( options.secure === true ? '; secure' : '' )
+            );
+        };
+        
+        /**
+        * parseCookies - retrieve document.cookie string and break it into a hash with values decoded and unserialized
+        *
+        * @access private
+        * @static
+        * @return OBJECT - hash of cookies from document.cookie
+        */
+        parseCookies = function()
+        {
+            var cookies = {}, i, pair, name, value, separated = document.cookie.split( ';' ), unparsedValue;
+            for( i = 0; i < separated.length; i = i + 1 )
+            {
+                pair = separated[i].split( '=' );
+                name = pair[0].replace( /^\s*/, '' ).replace( /\s*$/, '' );
+
+                try
+                {
+                    value = decodeURIComponent( pair[1] );
+                }
+                catch( e1 )
+                {
+                    value = pair[1];
+                }
+
+                if( typeof JSON === 'object' && JSON !== null && typeof JSON.parse === 'function' )
+                {
+                    try
+                    {
+                        unparsedValue = value;
+                        value = JSON.parse( value );
+                    }
+                    catch( e2 )
+                    {
+                        value = unparsedValue;
+                    }
+                }
+
+                cookies[name] = value;
+            }
+            return cookies;
+        };
+
+        constructor = function(){};
+
+        /**
+         * get - get one, several, or all cookies
+         *
+         * @access public
+         * @paramater Mixed cookieName - String:name of single cookie; Array:list of multiple cookie names; Void (no param):if you want all cookies
+         * @return Mixed - Value of cookie as set; Null:if only one cookie is requested and is not found; Object:hash of multiple or all cookies (if multiple or all requested);
+         */
+        constructor.prototype.get = function( cookieName )
+        {
+            var returnValue, item, cookies = parseCookies();
+
+            if( typeof cookieName === 'string' )
+            {
+                returnValue = ( typeof cookies[cookieName] !== 'undefined' ) ? cookies[cookieName] : null;
+            }
+            else if( typeof cookieName === 'object' && cookieName !== null )
+            {
+                returnValue = {};
+                for( item in cookieName )
+                {
+                    if( typeof cookies[cookieName[item]] !== 'undefined' )
+                    {
+                        returnValue[cookieName[item]] = cookies[cookieName[item]];
+                    }
+                    else
+                    {
+                        returnValue[cookieName[item]] = null;
+                    }
+                }
+            }
+            else
+            {
+                returnValue = cookies;
+            }
+
+            return returnValue;
+        };
+        
+        /**
+         * filter - get array of cookies whose names match the provided RegExp
+         *
+         * @access public
+         * @paramater Object RegExp - The regular expression to match against cookie names
+         * @return Mixed - Object:hash of cookies whose names match the RegExp
+         */
+        constructor.prototype.filter = function( cookieNameRegExp )
+        {
+            var cookieName, returnValue = {}, cookies = parseCookies();
+
+            if( typeof cookieNameRegExp === 'string' )
+            {
+                cookieNameRegExp = new RegExp( cookieNameRegExp );
+            }
+
+            for( cookieName in cookies )
+            {
+                if( cookieName.match( cookieNameRegExp ) )
+                {
+                    returnValue[cookieName] = cookies[cookieName];
+                }
+            }
+
+            return returnValue;
+        };
+        
+        /**
+         * set - set or delete a cookie with desired options
+         *
+         * @access public
+         * @paramater String cookieName - name of cookie to set
+         * @paramater Mixed value - Any JS value. If not a string, will be JSON encoded; NULL to delete
+         * @paramater Object options - optional list of cookie options to specify
+         * @return void
+         */
+        constructor.prototype.set = function( cookieName, value, options )
+        {
+            if( typeof options !== 'object' || options === null )
+            {
+                options = {};
+            }
+
+            if( typeof value === 'undefined' || value === null )
+            {
+                value = '';
+                options.expiresAt = -8760;
+            }
+
+            else if( typeof value !== 'string' )
+            {
+                if( typeof JSON === 'object' && JSON !== null && typeof JSON.stringify === 'function' )
+                {
+                    value = JSON.stringify( value );
+                }
+                else
+                {
+                    throw new Error( 'cookies.set() received non-string value and could not serialize.' );
+                }
+            }
+
+
+            var optionsString = assembleOptionsString( options );
+
+            document.cookie = cookieName + '=' + encodeURIComponent( value ) + optionsString;
+        };
+        
+        /**
+         * del - delete a cookie (domain and path options must match those with which the cookie was set; this is really an alias for set() with parameters simplified for this use)
+         *
+         * @access public
+         * @paramater MIxed cookieName - String name of cookie to delete, or Bool true to delete all
+         * @paramater Object options - optional list of cookie options to specify ( path, domain )
+         * @return void
+         */
+        constructor.prototype.del = function( cookieName, options )
+        {
+            var allCookies = {}, name;
+
+            if( typeof options !== 'object' || options === null )
+            {
+                options = {};
+            }
+
+            if( typeof cookieName === 'boolean' && cookieName === true )
+            {
+                allCookies = this.get();
+            }
+            else if( typeof cookieName === 'string' )
+            {
+                allCookies[cookieName] = true;
+            }
+
+            for( name in allCookies )
+            {
+                if( typeof name === 'string' && name !== '' )
+                {
+                    this.set( name, null, options );
+                }
+            }
+        };
+        
+        /**
+         * test - test whether the browser is accepting cookies
+         *
+         * @access public
+         * @return Boolean
+         */
+        constructor.prototype.test = function()
+        {
+            var returnValue = false, testName = 'cT', testValue = 'data';
+
+            this.set( testName, testValue );
+
+            if( this.get( testName ) === testValue )
+            {
+                this.del( testName );
+                returnValue = true;
+            }
+
+            return returnValue;
+        };
+        
+        /**
+         * setOptions - set default options for calls to cookie methods
+         *
+         * @access public
+         * @param Object options - list of cookie options to specify
+         * @return void
+         */
+        constructor.prototype.setOptions = function( options )
+        {
+            if( typeof options !== 'object' )
+            {
+                options = null;
+            }
+
+            defaultOptions = resolveOptions( options );
+        };
+
+        return new constructor();
+    } )();
+    
+    gtnamespace.theme = ( function() {
+        var constructor, defaultTheme = {
+            name: 'Theme Default',
+            fontFamily: 'Verdana',
+            commentBodyGradient: true,
+            commentBodyColorStart: '#FFFFFF',
+            commentBodyColorEnd: '#BACDDD'
+        },
+        SJTheme = {
+            name: 'Theme SamuraiJack',
+            fontFamily: 'Arial,Helvetica,sans-serif',
+            commentBodyGradient: false,
+            commentBodyColorStart: '#FFFFFF',
+            commentBodyColorEnd: '#EAF2F9'
+        },
+        cookieCurrentTheme = 'gt-current-theme';
+        
+        constructor = function(){};
+        
+        constructor.prototype.getCurrentTheme = function() {
+            var currentTheme = gtnamespace.cookies.get(cookieCurrentTheme);
+            if (currentTheme == null) {
+                return defaultTheme;
+            } else {
+                return currentTheme;
+            }
+            
+        };
+        
+        constructor.prototype.init = function() {
+/*
+<div class="user_theme_block">
+	<div class="user_theme_current">Theme Default</div>
+	<div class="clear"></div>
+    <div class="user_theme_settings">
+        <select>
+            <option value="0">Theme Default</option>
+            <option value="1">My Theme</option>
+        </select>
+    </div>
+    <div class="clear"></div>
+</div>
+*/
+            var currentTheme = this.getCurrentTheme();
+            var userThemeBlock = $('<div class="user_theme_block"></div>');
+            var startPositionForThemeBlock=$('div.user_logined_block')[0] || $('div.auth_actions')[0];
+            userThemeBlock.insertAfter(startPositionForThemeBlock);
+            
+            var headerThemeBlock = $('<div class="user_theme_current"></div>');
+        };
+        
+    } )();
+    
+    gtnamespace.cookies.set('property',obj1);
+    gtnamespace.cookies.set('theme-currnet','myTheme');
+    
+    console.log(gtnamespace.cookies.get());
+    
+    /* our styles */
+    var styles = $('<style type="text/css" />').appendTo('head');
+    styles.html('.user_theme_block {background: -moz-linear-gradient(center top , #F0DE39 0%, #F0DE39 2%, #F9D857 50%, #F9D650 51%, #F9D650 100%) repeat scroll 0 0 transparent;border-radius: 10px 10px 10px 10px;box-shadow: 0 0 3px 1px #F9E350 inset;margin: 0 0 1.5em;padding: 10px;position: relative;}');
+    
+    var currentTheme = gtnamespace.theme.getCurrentTheme();
+  
     /* last comment in our news table*/
     window.lastCommentsShow = function(onNewsPage) {
         $('table.news_shortlist a, div.news_list .item h3 a').each(function(){
@@ -113,7 +531,7 @@ function main() {
 
     $('.news_list .item').css('font-size', '14px');
     $('.news_list .item h3').css({'font-size':'17px','font-weight':'bold'});
-    $('div.g960').css({'background': '#F9FBFB','font-family':'Verdana'});
+    $('div.g960').css({'background': '#F9FBFB','font-family':currentTheme.fontFamily});
     $('.news_list .commentaries .item div.spoiler').css('background', '#F9FBFB');
      $('.news_list .item .clear').css({'border': 'none'});
      $('.news_list .item div.offtopic').css({'font-size': '12px !important'});
@@ -188,7 +606,7 @@ function main() {
     window.lastCommentsShow();
  }
  else {
-     $('div.g960').css({'background': '#F9FBFB','font-family':'Verdana','font-size':'12px'});
+     $('div.g960').css({'background': '#F9FBFB','font-family':currentTheme.fontFamily,'font-size':'12px'});
      $('div.left_col table.news_shortlist').load('http://www.gametech.ru/news/26889/ table.news_shortlist>tbody', function(res){
          window.lastCommentsShow();
      });
